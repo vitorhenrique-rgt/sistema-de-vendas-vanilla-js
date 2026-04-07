80b377f4247f7d19a0b913fe202ee1c4e8e5b4c5
Vitor <vitorhenrique_rgt@outlook.com.br>
2026-04-06 22:47:14 -0300

    feat: add product form with validation and state persistence

--- js/main.js
+++ js/main.js
@@ -1,1 +1,38 @@
-console.log("script main.js rodando")
+const productForm = document.getElementById('product-form')
+const productName = document.getElementById('product-name')
+const productDescription = document.getElementById('product-description')
+const productCost = document.getElementById('product-cost')
+const productPrice = document.getElementById('product-price')
+
+let products = []
+
+function addProduct() {
+  if (productName.value === "" || productCost.value === "" || productPrice.value === "") {
+    console.log("Campos Obrigatórios")
+  } else {
+    const product = {
+      id: crypto.randomUUID(),
+      name: productName.value,
+      description: productDescription.value,
+      cost: Number(productCost.value),
+      price: Number(productPrice.value)
+    }
+    products.push(product)
+    console.log(products)
+  }
+}
+
+function cleanProductForm() {
+  productName.value = ""
+  productDescription.value = ""
+  productCost.value = ""
+  productPrice.value = ""
+}
+productForm.addEventListener('submit', (e) => {
+  e.preventDefault()
+  addProduct()
+  cleanProductForm()
+})
+
+
+
